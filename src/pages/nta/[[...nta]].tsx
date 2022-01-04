6cc4b507b79fadde51e4440fdb60ff061dc57ee5
Tyler Matteo <tylermatteo434@gmail.com>
2022-01-03 21:10:50 -0500

    Add stubbed getServerSideProps function

--- src/pages/nta/[[...nta]].tsx
+++ src/pages/nta/[[...nta]].tsx
@@ -1,12 +1,30 @@
 import { Box, Flex } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 import { Map } from "@components/Map";
 import { Header } from "@components/Header";
 import { Legend } from "@components/Legend";
 import { IndicatorPanel } from "@components/IndicatorPanel";
 
-// export interface NtaProps {
-//   initialNtaIndicatorRecord: NtaIndicatorRecord | null;
-// }
+export interface NtaProps {
+  initialNta: string | null;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  if (!context.params) {
+    return {
+      props: {
+        initialNta: null,
+      },
+    };
+  }
+  const { nta } = context.params;
+  const selectedNtaId: string | null = nta && nta?.length > 0 ? nta[0] : null;
+  return {
+    props: {
+      initialNta: selectedNtaId,
+    },
+  };
+};
 
 // export const getServerSideProps: GetServerSideProps = async (context) => {
 //   return
@@ -62,20 +80,23 @@ import { IndicatorPanel } from "@components/IndicatorPanel";
 //   return { props };
 // };
 
-const Nta = () => (
-  <Box height="100vh">
-    <Header />
-    <Map />
-    <Flex direction="column" justify="end" height="100%">
-      <Legend
-        position={["relative", "absolute"]}
-        left={["auto", 8]}
-        bottom={["auto", 8]}
-        w={["100%", "215px"]}
-      />
-      <IndicatorPanel />
-    </Flex>
-  </Box>
-);
+const Nta = ({ initialNta }: NtaProps) => {
+  console.log(initialNta);
+  return (
+    <Box height="100vh">
+      <Header />
+      <Map />
+      <Flex direction="column" justify="end" height="100%">
+        <Legend
+          position={["relative", "absolute"]}
+          left={["auto", 8]}
+          bottom={["auto", 8]}
+          w={["100%", "215px"]}
+        />
+        <IndicatorPanel />
+      </Flex>
+    </Box>
+  );
+};
 
 export default Nta;
