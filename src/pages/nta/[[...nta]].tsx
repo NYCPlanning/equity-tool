a6793333f017715fb05c10011d4d70cab942f1fa
Tyler Matteo <tylermatteo434@gmail.com>
2022-01-05 14:56:41 -0500

    Add job for version and prod deploy

--- src/pages/nta/[[...nta]].tsx
+++ src/pages/nta/[[...nta]].tsx
@@ -26,62 +26,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   };
 };
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   return
-//   if (!context.params) {
-//     return {
-//       props: {
-//         initialNtaIndicatorRecord: null,
-//       },
-//     };
-//   }
-//   const { nta } = context.params;
-//   const selectedNtaId: string | null = nta && nta?.length > 0 ? nta[0] : null;
-//   const fs = require("fs"); // eslint-disable-line @typescript-eslint/no-var-requires
-//   const path = require("path"); // eslint-disable-line @typescript-eslint/no-var-requires
-
-//   // This function is a helper that makes a type-safe wrapper for Object.hasOwnProperty
-//   // Normally we would move this somewhere sharable but since most of this code will likely only
-//   // be temporary, it's fine for now
-//   function hasOwnProperty<T>(obj: T, key: PropertyKey): key is keyof T {
-//     return Object.prototype.hasOwnProperty.call(obj, key);
-//   }
-
-//   const props: NtaProps = {
-//     initialNtaIndicatorRecord: null,
-//   };
-
-//   const parseNtaJson = () => {
-//     return new Promise<NtaIndicatorDict>((accept, reject) => {
-//       fs.readFile(
-//         path.resolve(`src/data/ntas.json`),
-//         "utf8",
-//         function (err: any, data: any) {
-//           if (err) {
-//             reject(err);
-//           }
-//           const ntaRawData: NtaIndicatorDict = JSON.parse(
-//             data.toString("utf8").replace(/^\uFEFF/, "")
-//           );
-//           accept(ntaRawData);
-//         }
-//       );
-//     });
-//   };
-
-//   const ntaIndicatorsData: NtaIndicatorDict = await parseNtaJson();
-//   if (
-//     selectedNtaId !== null &&
-//     hasOwnProperty(ntaIndicatorsData, selectedNtaId)
-//   ) {
-//     props.initialNtaIndicatorRecord = ntaIndicatorsData[selectedNtaId];
-//   }
-
-//   return { props };
-// };
-
 const Nta = ({ initialNta }: NtaProps) => {
-  console.log(initialNta);
+  console.log({ initialNta });
   return (
     <Box height="100vh">
       <Header />
