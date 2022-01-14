a33c945504191475ac2fa0e21f12b112a4990968
test <tylermatteo434@gmail.com>
2022-01-14 15:00:47 -0500

    Fix Map and ref typings

--- src/pages/map/[[...subroutes]].tsx
+++ src/pages/map/[[...subroutes]].tsx
@@ -36,7 +36,7 @@ const MapPage = () => {
 
   const indicatorRecord = useIndicatorRecord(geoid);
 
-  const mapContainer = useRef(null);
+  const mapContainer = useRef<HTMLDivElement>(null);
 
   return (
     <Flex height="100vh" direction="column" bg="gray.100">
@@ -54,7 +54,10 @@ const MapPage = () => {
             height="100%"
             rounded="lg"
           >
-            <Map layers={layers} mapParent={mapContainer} />
+            <Map
+              layers={layers ? layers : undefined}
+              parent={mapContainer.current ? mapContainer.current : undefined}
+            />
           </Box>
         </Box>
       </Flex>
