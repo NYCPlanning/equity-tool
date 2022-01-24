debe034f4ae0cf0c1571caf49abc12d4a9696964
godfreyyeung <godfreyyeung@gmail.com>
2022-01-24 10:59:03 -0500

    6308 Use Center instead of spacer divs

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,18 +1,19 @@
 import { Flex, Box, Center } from "@chakra-ui/react";
 
+// layout probably still needs rework
 const AboutPage = () => {
   return (
     <Flex direction="column" width="100%">
-      <Flex direction="row" bg="teal" height="300px">
-        <Box flex="1"></Box>
-        <Box flex="5">
-          <h1>About</h1>
-        </Box>
-        <Box flex="2">
-          <h2>Feedback</h2>
-        </Box>
-        <Box flex="1"></Box>
-      </Flex>
+      <Center height="300px" bg="teal">
+        <Flex direction="row" width={["100%", "50%"]}>
+          <Box flex="5">
+            <h1>About</h1>
+          </Box>
+          <Box flex="2">
+            <h2>Feedback</h2>
+          </Box>
+        </Flex>
+      </Center>
       <Box>
         <Center width="100%">
           <Flex direction="row">
