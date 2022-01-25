a26fb1fab2227a486e833f6553f8f00a4e44abce
godfreyyeung <godfreyyeung@gmail.com>
2022-01-25 14:57:54 -0500

    6390 Adjust tests for 6390

--- cypress/integration/pages/Map/[[...subroutes]].spec.ts
+++ cypress/integration/pages/Map/[[...subroutes]].spec.ts
@@ -1,6 +1,6 @@
 describe("Map catch-all page", () => {
   beforeEach(() => {
-    cy.visit("/map");
+    cy.visit("/map/datatool");
   });
 
   it("should have a header", () => {
@@ -8,17 +8,17 @@ describe("Map catch-all page", () => {
   });
 
   it("should switch geography when user uses Geography Select toolbar", () => {
-    cy.url().should("include", "/map");
+    cy.url().should("include", "/map/datatool");
 
     cy.contains("Census").click();
 
-    cy.url().should("include", "/map/census");
+    cy.url().should("include", "/map/datatool/census");
 
     cy.contains("Borough").click();
 
-    cy.url().should("include", "/map/borough");
+    cy.url().should("include", "/map/datatool/borough");
 
-    cy.visit("/map/census");
+    cy.visit("/map/datatool/census");
 
     cy.contains("Census Area").should("have.attr", "data-active");
   });
