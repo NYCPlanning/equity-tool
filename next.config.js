ff513a753d1d343ca945ca372c47d3d092ec3676
godfreyyeung <godfreyyeung@gmail.com>
2022-01-26 16:53:33 -0500

    6389 Update default redirect to datatool route

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: "/",
-        destination: "/map",
+        destination: "/map/datatool/census",
         permanent: true,
       },
     ];
