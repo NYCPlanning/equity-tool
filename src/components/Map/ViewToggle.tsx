cbe1804789b61d9eb708f3a7ada8bdbf60d603c7
godfreyyeung <godfreyyeung@gmail.com>
2022-02-07 15:58:17 -0500

    6587 Revert Mobile ViewToggle buttons to StyleProps

--- src/components/Map/ViewToggle.tsx
+++ src/components/Map/ViewToggle.tsx
@@ -22,13 +22,22 @@ export const ViewToggle = ({
         direction="row"
         position="fixed"
         width="100%"
+        height="3.75rem"
         bottom="0"
         left="0"
         zIndex="999"
       >
         <Button
           flex="1"
-          variant="bigToggle"
+          variant="toggle"
+          backgroundColor="gray.50"
+          height="100%"
+          borderRadius=".5rem 0 0 0"
+          _active={{
+            backgroundColor: "teal.50",
+            color: "teal",
+            border: "1px solid teal",
+          }}
           onClick={onDataToolClick}
           isActive={view === "datatool"}
           isFullWidth
@@ -38,7 +47,15 @@ export const ViewToggle = ({
         </Button>
         <Button
           flex="1"
-          variant="bigToggle"
+          variant="toggle"
+          backgroundColor="gray.50"
+          height="100%"
+          borderRadius="0 .5rem 0 0"
+          _active={{
+            backgroundColor: "teal.50",
+            color: "teal",
+            border: "1px solid teal",
+          }}
           onClick={onDriClick}
           isActive={view === "dri"}
           isFullWidth
