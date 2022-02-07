8d5f05b5bd7ca7f48a6ac3456d3e739e6654a2dd
godfreyyeung <godfreyyeung@gmail.com>
2022-02-07 13:27:30 -0500

    6587 Move mobile ViewToggle code into ViewToggle component

--- src/components/Map/ViewToggle.tsx
+++ src/components/Map/ViewToggle.tsx
@@ -1,4 +1,4 @@
-import { BoxProps, Button } from "@chakra-ui/react";
+import { Flex, Box, BoxProps, Button } from "@chakra-ui/react";
 import { ToggleButtonGroup } from "@components/ToggleButtonGroup";
 
 interface ViewToggleProps extends BoxProps {
@@ -11,28 +11,78 @@ export const ViewToggle = ({
   onDataToolClick,
   onDriClick,
   view,
-  ...boxProps
 }: ViewToggleProps) => {
   return (
-    <ToggleButtonGroup {...boxProps}>
-      <Button
-        onClick={onDataToolClick}
-        isActive={view === "datatool"}
-        variant="toggle"
-        data-cy="dataToolBtn"
-        data-cy-context="desktop"
+    <>
+      <Flex
+        display={{
+          base: "flex",
+          lg: "none",
+        }}
+        direction="row"
+        position="fixed"
+        width="100%"
+        height="3.75rem"
+        bottom="0"
+        left="0"
+        zIndex="999"
       >
-        Data Tool
-      </Button>
-      <Button
-        onClick={onDriClick}
-        isActive={view === "dri"}
-        variant="toggle"
-        data-cy="driBtn"
-        data-cy-context="desktop"
+        <Box flex="1">
+          <Button
+            onClick={onDataToolClick}
+            isActive={view === "datatool"}
+            height="100%"
+            isFullWidth
+            data-cy="dataToolBtn"
+            data-cy-context="mobile"
+          >
+            Data Tool
+          </Button>
+        </Box>
+        <Box flex="1">
+          <Button
+            onClick={onDriClick}
+            isActive={view === "dri"}
+            height="100%"
+            isFullWidth
+            data-cy="driBtn"
+            data-cy-context="mobile"
+          >
+            Displacement Risk Index
+          </Button>
+        </Box>
+      </Flex>
+
+      <ToggleButtonGroup
+        position="absolute"
+        top={5}
+        left={8}
+        zIndex={200}
+        boxShadow="lg"
+        display={{
+          base: "none",
+          lg: "block",
+        }}
       >
-        Displacement Risk Index
-      </Button>
-    </ToggleButtonGroup>
+        <Button
+          onClick={onDataToolClick}
+          isActive={view === "datatool"}
+          variant="toggle"
+          data-cy="dataToolBtn"
+          data-cy-context="desktop"
+        >
+          Data Tool
+        </Button>
+        <Button
+          onClick={onDriClick}
+          isActive={view === "dri"}
+          variant="toggle"
+          data-cy="driBtn"
+          data-cy-context="desktop"
+        >
+          Displacement Risk Index
+        </Button>
+      </ToggleButtonGroup>
+    </>
   );
 };
