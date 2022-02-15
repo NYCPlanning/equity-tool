ee6a5a6fba91b6623e48ef1822a44e9045c94c09
godfreyyeung <godfreyyeung@gmail.com>
2022-02-15 15:21:29 -0500

    6805 Return object from useMapSubrouteInfo

--- src/hooks/useMapSubrouteInfo/useMapSubrouteInfo.ts
+++ src/hooks/useMapSubrouteInfo/useMapSubrouteInfo.ts
@@ -2,31 +2,39 @@ import { useRouter } from "next/router";
 
 type ViewValue = "dri" | "datatool" | null;
 
-type SubrouteInfoArray = [ViewValue, ...Array<string | null>];
+interface SubrouteInfo {
+  view: ViewValue;
+  geography: string | null;
+  geoid: string | null;
+}
 
 // Use under Map/ route to acquire subroute view, geography, geoid info
-export const useMapSubrouteInfo = ():SubrouteInfoArray | null => {
+export const useMapSubrouteInfo = (): SubrouteInfo => {
   const router = useRouter();
 
   const { subroutes } = router.query;
 
-  const [viewParam, geographyParam, geoidParam] = subroutes ? subroutes : [null, null, null];
+  const [viewParam, geographyParam, geoidParam] = subroutes
+    ? subroutes
+    : [null, null, null];
 
-  const view = typeof viewParam === "string"
-    ? viewParam === "dri" || viewParam === "datatool"
-      ? viewParam
-      : null
-    : viewParam !== null && viewParam !== undefined
-    ? viewParam[0]
-    : null;
+  const view =
+    typeof viewParam === "string"
+      ? viewParam === "dri" || viewParam === "datatool"
+        ? viewParam
+        : null
+      : viewParam !== null && viewParam !== undefined
+      ? viewParam[0]
+      : null;
 
-  const geography = typeof geographyParam === "string"
-    ? geographyParam
-    : geographyParam !== null && geographyParam !== undefined
-    ? geographyParam[0]
-    : null;
+  const geography =
+    typeof geographyParam === "string"
+      ? geographyParam
+      : geographyParam !== null && geographyParam !== undefined
+      ? geographyParam[0]
+      : null;
 
   const geoid = geoidParam ? geoidParam : null;
 
-  return [view, geography, geoid];
+  return { view, geography, geoid };
 };
