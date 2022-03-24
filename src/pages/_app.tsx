88dc8724d7a7348d7b84f0816c27ff63d88d6ee4
godfreyyeung <godfreyyeung@gmail.com>
2022-03-24 14:59:08 -0400

    7554 Hide app container scrollbars on Map page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,14 @@ import theme from "../theme";
 import { AppProps } from "next/app";
 import { PageHeader } from "@components/PageHeader";
 import ReactGA from "react-ga4";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   ReactGA.initialize("G-DEJNY15B0G");
+
+  const router = useRouter();
+
+  const isMapRoute = router.pathname.startsWith("/map");
 
   return (
     <ChakraProvider resetCSS theme={theme}>
@@ -17,7 +22,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Flex height="100vh" direction="column">
         <Header />
 
-        <Box height="calc(100vh - 4.375rem)" overflow="auto">
+        <Box
+          height="calc(100vh - 4.375rem)"
+          overflow={isMapRoute ? "hidden" : "auto"}
+        >
           <Flex direction="row" height="100%">
             <Component {...pageProps} />
           </Flex>
