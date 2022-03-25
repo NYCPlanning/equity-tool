0e2c2bfd76f13827ddafea8ee04664abe51890e2
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2022-03-25 16:10:38 -0400

    Updated indicator styling

--- src/components/SidebarContent/DRISelection/SubindicatorBin.tsx
+++ src/components/SidebarContent/DRISelection/SubindicatorBin.tsx
@@ -9,31 +9,61 @@ export const SubindicatorBin = ({ bin }: SubindicatorBinProps) => {
   switch (bin) {
     case "Highest":
       return (
-        <Tag ml="1rem" background="#772F7A" color="white" borderRadius="25px">
+        <Tag
+          ml="1rem"
+          background="rgba(119, 47, 122, 0.2)"
+          color="gray.700"
+          border="1px solid rgba(119, 47, 122, 1)"
+          borderRadius="25px"
+        >
           {bin}
         </Tag>
       );
     case "Higher":
       return (
-        <Tag ml="1rem" background="#BD2E89" color="white" borderRadius="25px">
+        <Tag
+          ml="1rem"
+          background="rgba(189, 46, 137, 0.2)"
+          color="gray.700"
+          border="1px solid rgba(189, 46, 137, 1)"
+          borderRadius="25px"
+        >
           {bin}
         </Tag>
       );
     case "Intermediate":
       return (
-        <Tag ml="1rem" background="#ED6CA0" color="white" borderRadius="25px">
+        <Tag
+          ml="1rem"
+          background="rgba(237, 108, 160, 0.2)"
+          color="gray.700"
+          border="1px solid rgba(237, 108, 160, 1)"
+          borderRadius="25px"
+        >
           {bin}
         </Tag>
       );
     case "Lower":
       return (
-        <Tag ml="1rem" background="#F5B6BC" color="white" borderRadius="25px">
+        <Tag
+          ml="1rem"
+          background="rgba(245, 182, 188, 0.2)"
+          color="gray.700"
+          border="1px solid rgba(245, 182, 188, 1)"
+          borderRadius="25px"
+        >
           {bin}
         </Tag>
       );
     case "Lowest":
       return (
-        <Tag ml="1rem" background="#FEEFE5" borderRadius="25px">
+        <Tag
+          ml="1rem"
+          background="rgba(254, 239, 229, 0.2)"
+          color="gray.700"
+          border="1px solid rgba(254, 239, 229, 1)"
+          borderRadius="25px"
+        >
           {bin}
         </Tag>
       );
