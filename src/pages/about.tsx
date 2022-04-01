775aa4e2e95c47f4a5b7c06d3d7efdaaeb209d99
horatio <horatiorosa@aol.com>
2022-04-01 16:57:34 -0400

    7781 About page copy updates  - added new copy explaining the Racial Equity Reports  - updating the Feedback email to edde@planning.nyc.gov

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -22,19 +22,24 @@ const AboutPage = () => (
         color="white"
         gridGap={20}
       >
-        <Box flex={{ base: "auto", xl: 2 }} py={10}>
-          <Heading size="lg" pb="0.5em">
+        <Box flex={{ base: "auto", md: 2, xl: 2 }} py={10}>
+          <Heading size="lg" fontSize="2.375rem" pb="0.5em">
             About
           </Heading>
-          <Text>
-            The NYC Department of City Planning (DCP) and Department of Housing
-            Preservation and Development (HPD) are introducing a new interactive
-            resource for New Yorkers who are interested in exploring the
-            city&apos;s housing conditions, demographic patterns, public health
-            and more.
+          <Text fontSize="1.5625rem">
+            The NYC Departments of City Planning (DCP) and Housing Preservation
+            and Development (HPD) are introducing a new interactive resource
+            that equips New Yorkers with data to navigate challenging
+            conversations about housing affordability, displacement, and racial
+            equity in our city.
           </Text>
         </Box>
-        <Feedback flex={1} py={10} display={{ base: "none", xl: "block" }} />
+        <Feedback
+          flex={1}
+          py={10}
+          fontSize="1rem"
+          display={{ base: "none", md: "block", xl: "block" }}
+        />
       </Flex>
     </Center>
 
@@ -63,19 +68,20 @@ const AboutPage = () => (
         </Box>
         <Box flex={{ base: "auto", lg: 2 }}>
           <Heading size="lg" pb="0.5em">
-            Equitable Data Development Tool
+            Community Data
           </Heading>
           <Text pb="0.5em">
-            This Equitable Data Development Tool will provide easy access to a
-            wide range of data to inform public discussions about racial equity
-            and planning for a fairer city. Whenever possible, this data is
-            broken down by race/ethnicity, so users can learn about
-            commonalities and disparities across neighborhoods and demographic
-            groups.
+            Community Data provides easy access to a wide range of demographic,
+            housing, and quality of life data to inform public discussions about
+            racial equity and planning for a fairer city. Whenever possible, the
+            data are broken down by neighborhood and race/ethnicity, to allow
+            New Yorkers to learn about commonalities and disparities across
+            neighborhoods and demographic groups.
           </Text>
-          <Text color="teal">
-            <Link href="/map" textDecoration="underline">
-              Go to the Data Tool
+          <Text color="black">
+            Go to{" "}
+            <Link href="/map" color="teal" textDecoration="underline">
+              Community Data
             </Link>
           </Text>
         </Box>
@@ -100,24 +106,78 @@ const AboutPage = () => (
         </Box>
         <Box flex={{ base: "auto", lg: 2 }}>
           <Heading size="lg" pb="0.5em">
-            Displacement Risk Index
+            Displacement Risk Map
           </Heading>
           <Text pb="0.5em">
-            The Displacement Risk Index will support the development of
-            strategies to ensure that New Yorkers can stay in their homes.
-            Displacement refers to the involuntary movement of an individual or
-            family from their home, whether as the result of demolition,
-            eviction, unaffordable housing costs, or poor-quality housing. By
-            tracking and combining a set of data points that may contribute to
-            New Yorkers&apos; risk of experiencing displacement, the Index gives
-            a general indication of the level of risk that residents of a
-            neighborhood face, as a group, in being displaced from their homes
-            relative to residents in other neighborhoods.
+            The Displacement Risk Map illustrates the level of risk residents
+            may face of being unable to stay in their home or neighborhood. The
+            map combines data about housing and demographics to indicate the
+            level of displacement risk in different neighborhoods as compared to
+            each other. It provides context for the consideration of proposed
+            projects and supports the continuing development of strategies to
+            help enable New Yorkers to stay in their homes and neighborhoods.
+            The map represents a snapshot of current conditions and does not
+            provide sufficient information to predict the effect that future
+            changes, such as changes to zoning or housing supply, would have on
+            any neighborhood.
           </Text>
-          <Text color="teal">
-            <Link href="/map/dri" textDecoration="underline">
-              Go to the Displacement Risk Index (DRI) Tool
+          <Text color="black">
+            Go to the{" "}
+            <Link href="/map/dri" color="teal" textDecoration="underline">
+              Displacement Risk Map
             </Link>
+          </Text>
+        </Box>
+      </Flex>
+      <Flex
+        gridGap={{ base: 8, lg: 6, xl: 12 }}
+        direction={{ base: "column", lg: "row" }}
+        maxW={{ base: "565px", lg: "1024px" }}
+      >
+        <Box flex={{ base: "auto", lg: 2 }}>
+          <Heading size="lg" pb="0.5em">
+            Racial Equity Reports
+          </Heading>
+          <Text pb="0.5em">
+            As of June 1, 2020,{" "}
+            <Link
+              href="https://nyco365.sharepoint.com/:b:/r/sites/NYCPLANNING/civic-engagement-studio/Shared%20Documents/03.%20Working%20Materials/03.%20%20Equity%20and%20Displacement%20Vulnerability/07.%20%20Key%20Presentations/UITF/Handout%20on%20Racial%20Equity%20Report%20applicability%20clean.pdf?csf=1&web=1&e=AD6tWT"
+              color="teal"
+              textDecoration="underline"
+            >
+              certain property owners
+            </Link>{" "}
+            applying for land use changes must produce a Racial Equity Report
+            using information pulled from the data tool. This report will assist
+            racial equity discussions during the public land use review process
+            known as{" "}
+            <Link
+              href="https://www.youtube.com/watch?v=G-k0kAn1GBQ"
+              color="teal"
+              textDecoration="underline"
+            >
+              ULURP
+            </Link>
+            . The Report must include: a community profile pulled from the data
+            tool, a narrative statement of how the project relates to the
+            City&apos;s goals and strategies to{" "}
+            <Link
+              href="https://wherewelive.cityofnewyork.us/"
+              color="teal"
+              textDecoration="underline"
+            >
+              affirmatively further fair housing
+            </Link>
+            , and project specific data about anticipated housing units and
+            jobs. See additional information about the Reports{" "}
+            <Link
+              href="/map/datatool/district"
+              color="teal"
+              textDecoration="underline"
+            >
+              here
+            </Link>
+            .
           </Text>
         </Box>
       </Flex>
@@ -136,17 +196,19 @@ const AboutPage = () => (
       >
         <Box flex={{ base: "auto", lg: 1 }} py={5} px={[10, 0]}>
           <Heading size="md" pb="0.5em">
-            Methods &amp; Sources
+            Methods and Sources
           </Heading>
           <Text pb="0.5em">
-            Local Law 78 of 2021 outlined a set of factors and data points to be
-            incorporated into the index, but directed the agencies to determine
-            the complete list and specify methodologies for how the data points
-            should be grouped and combined.
+            Local Law 78 of 2021 outlined a set of indicators to be incorporated
+            into the Equitable Development Data Explorer, and directed the DCP
+            and HPD to determine the complete data points and specify
+            methodologies for how the data points should be incorporated into a
+            displacement risk map.
           </Text>
-          <Text color="teal">
-            <Link href="/methods" textDecoration="underline">
-              Read more about our methodologies
+          <Text color="black">
+            Read more about the{" "}
+            <Link href="/methods" color="teal" textDecoration="underline">
+              methodologies
             </Link>
           </Text>
         </Box>
@@ -156,17 +218,19 @@ const AboutPage = () => (
           </Heading>
           <Text pb="0.5em">
             The Bibliography contains resources reviewed by staff at DCP and HPD
-            during the development of the data tool. These resources reflect
+            during the development of the Data Explorer. These resources reflect
             decades of wide-ranging thought and research on social and racial
             equity, housing policy, and neighborhood change.
           </Text>
-          <Text color="teal">
+          <Text color="black">
+            Open the{" "}
             <Link
               href="https://www1.nyc.gov/assets/planning/download/pdf/planning-level/housing-economy/eddt-bibliography.pdf"
               textDecoration="underline"
+              color="teal"
               isExternal
             >
-              Open the Bibliography <ExternalLinkIcon mx="2px" />
+              Bibliography <ExternalLinkIcon mx="2px" />
             </Link>
           </Text>
         </Box>
@@ -175,16 +239,18 @@ const AboutPage = () => (
             Data Dictionary
           </Heading>
           <Text pb="0.5em">
-            The data dictionary is a set of information describing the data
-            points and methodologies included in the data tool.
+            The Data Dictionary is a set of information describing the data
+            points, sources, and methodologies included in the Data Explorer.
           </Text>
-          <Text color="teal">
+          <Text color="black">
+            Open the{" "}
             <Link
               href="https://www1.nyc.gov/assets/planning/download/pdf/planning-level/housing-economy/eddt-data-dictionary.pdf"
               textDecoration="underline"
+              color="teal"
               isExternal
             >
-              Open the Data Dictionary <ExternalLinkIcon mx="2px" />
+              Data Dictionary <ExternalLinkIcon mx="2px" />
             </Link>
           </Text>
         </Box>
@@ -199,7 +265,7 @@ const AboutPage = () => (
     </Center>
     <Center px={{ base: 4, lg: 6 }}>
       <Feedback
-        display={{ base: "block", xl: "none" }}
+        display={{ base: "block", md: "none", xl: "none" }}
         maxW={{ base: "565px", lg: "1024px" }}
       />
     </Center>
