a4354eaeaa340fd8d9de8505dc7df196d8d5d95a
test <tylermatteo434@gmail.com>
2022-05-09 12:07:19 -0400

    Remove indicator and estimate boilerplate types, hook, and data

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,1 @@
-export * from "./useIndicatorRecord";
 export * from "./useLayers";
--- test/jest.config.js
+++ test/jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
     "^@components/(.*)$": "<rootDir>/src/components/$1",
     "^@data/(.*)$": "<rootDir>/src/data/$1",
     "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
-    "^@type/(.*)$": "<rootDir>/src/type/$1",
     "^@helpers/(.*)$": "<rootDir>/src/helpers/$1",
     "^@constants/(.*)$": "<rootDir>/src/constants/$1",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
