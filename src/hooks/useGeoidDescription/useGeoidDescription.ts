a89cf730825932b23206df0c6e5b69968f283702
Tyler Matteo <tylermatteo434@gmail.com>
2022-10-07 13:59:23 -0400

    Task/10658 data page sticky header (#185)
    * Working on data page header redesign. Adds new SubgroupMenu component
    
    * Finishing up mobile size sticky behavior
    
    * Fix subgroup buttons not being disabled for hopd
    
    * Make geography label not bold in data page header
    
    * Change tooltip to popover, some styling tweaks
    
    * Change header back to gray.700 color

---
+++ src/hooks/useGeoidDescription/useGeoidDescription.ts
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { useGeography } from "@hooks/useGeography";
+import { useGeoid } from "@hooks/useGeoid/useGeoid";
+import { usePumaInfo } from "@hooks/usePumaInfo";
+import { Geography } from "@constants/geography";
+import { getBoroughName } from "@helpers/getBoroughName";
+import { fetchNtaInfo } from "@helpers/fetchNtaInfo";
+
+export interface GeoidDescription {
+  id: string;
+  label: string;
+}
+
+export const useGeoidDescription = (): GeoidDescription => {
+  const { DISTRICT, BOROUGH, CITYWIDE, NTA } = Geography;
+  const geography = useGeography();
+  const geoid = useGeoid();
+
+  const pumaInfo = usePumaInfo();
+
+  const [ntaInfo, setNtaInfo] = useState({
+    ntaname: "",
+    ntacode: "",
+  });
+
+  useEffect(() => {
+    geography === Geography.NTA &&
+      fetchNtaInfo(geoid, (ntaInfo: any) => {
+        setNtaInfo(ntaInfo);
+      });
+  }, [geoid]);
+
+  if (geoid === null) {
+    return {
+      id: "",
+      label: "",
+    };
+  }
+
+  switch (geography) {
+    case DISTRICT:
+      return {
+        id: `PUMA ${geoid}`,
+        label: pumaInfo?.neighborhoods ? pumaInfo.neighborhoods : "",
+      };
+    case BOROUGH:
+      return {
+        id: "",
+        label: getBoroughName(geoid),
+      };
+    case CITYWIDE:
+      return {
+        id: "",
+        label: "Citywide",
+      };
+    case NTA:
+      // primaryHeading = `${ntaInfo?.ntaname}`;
+      return {
+        id: `NTA: ${geoid}`,
+        label: ntaInfo.ntaname,
+      };
+    default:
+      return {
+        id: "",
+        label: "",
+      };
+  }
+};
