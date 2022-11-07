80b89964f5f69613ffbb3810c651f3f33e6a4f56
Bryan Marchena <bmarchena@planning.nyc.gov>
2022-11-07 09:44:38 -0500

    Add useWindowDimensions hook

---
+++ src/hooks/useWindowDimensions/useWindowDimensions.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+interface WindowDimensions {
+  width: number | undefined;
+  height: number | undefined;
+}
+
+export const useWindowDimensions = (): WindowDimensions => {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimensions>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return (): void => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
