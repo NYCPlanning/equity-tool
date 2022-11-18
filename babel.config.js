96461411ded7001ad97b5ab8f4fde186529b7eb2
test <tylermatteo434@gmail.com>
2022-11-18 10:57:32 -0500

    Streamline babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,1 @@
-module.exports = (api) => {
-  const isTest = api.env("test");
-  return isTest
-    ? { presets: ["next/babel", "@babel/preset-typescript"] }
-    : {
-        presets: [
-          "next/babel",
-          // ["@babel/preset-env", { targets: { node: "16.13" } }],
-          "@babel/preset-typescript",
-        ],
-      };
-};
+module.exports = { presets: ["next/babel", "@babel/preset-typescript"] };
