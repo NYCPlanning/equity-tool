f3499c5e93e1cb5f5aab5a975e2aadc26c05b534
tangoyankee <miller.tim108@gmail.com>
2023-02-28 11:21:26 -0500

    arrange table headers

--- src/components/VintageTable/VintageTableDesktop.tsx
+++ src/components/VintageTable/VintageTableDesktop.tsx
@@ -1,9 +1,41 @@
-import { Table, Tbody, Thead } from "@chakra-ui/react";
+import { Table, Tbody, Thead, Th, Tr } from "@chakra-ui/react";
+import { Vintage } from "@schemas/vintage";
+import { HeaderCell } from "@schemas/headerCell";
 
-const VintageTableDesktop = () => {
+export interface VintageTableDesktopProps {
+  vintages: Vintage[];
+}
+
+const VintageTableDesktop = ({ vintages }: VintageTableDesktopProps) => {
+  const vintageLabels = vintages.map((vintage) => vintage.label);
+  const vintagesHeaderRows: Array<HeaderCell[]> = [];
+  vintages.forEach((vintage) => {
+    vintage.headers.forEach((headerRow, i) => {
+      const vintagesHeaderRow = vintagesHeaderRows[i];
+      vintagesHeaderRow
+        ? (vintagesHeaderRows[i] = vintagesHeaderRow.concat(headerRow))
+        : (vintagesHeaderRows[i] = headerRow);
+    });
+  });
+
   return (
     <Table variant="striped">
-      <Thead></Thead>
+      <Thead>
+        <Tr>
+          {vintageLabels.map((label) => (
+            <Th key={`vintage-${label}`}>{label}</Th>
+          ))}
+        </Tr>
+        {vintagesHeaderRows.map((headerCells, i) => (
+          <Tr key={`vintage-row-${i}`}>
+            {headerCells.map((headerCell, i) => (
+              <Th key={`header-cell-${headerCell.label}-${i}`}>
+                {headerCell.label}
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
       <Tbody></Tbody>
     </Table>
   );
--- src/schemas/vintage.ts
+++ src/schemas/vintage.ts
@@ -1,23 +1,13 @@
-import { object, string, array, number, boolean, InferType } from "yup";
+import { object, string, array, boolean, InferType } from "yup";
 import { rowSchema } from "@schemas/row";
+import { headerCellSchema } from "@schemas/headerCell";
 
 export const vintageSchema = object({
   label: string().required(),
   placeholder: string().optional(),
   rows: array().of(rowSchema).required(),
   isChange: boolean().required(),
-  headers: array()
-    .of(
-      array()
-        .of(
-          object({
-            label: string().required(),
-            colspan: number().required(),
-          })
-        )
-        .required()
-    )
-    .required(),
+  headers: array().of(array().of(headerCellSchema).required()).required(),
 });
 
 export type Vintage = InferType<typeof vintageSchema>;
