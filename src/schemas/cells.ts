fda2f38a77ee53ae90d3c879c8d5592a8e39cbf4
tangoyankee <miller.tim108@gmail.com>
2023-03-02 11:18:36 -0500

    remove first cell hider. remove cells schema definition. create key for null datapoints

--- src/schemas/cells.ts
+++
@@ -1,6 +0,0 @@
-import { array, InferType } from "yup";
-import { dataPointSchema } from "@schemas/dataPoint";
-
-export const cellsSchema = array().of(dataPointSchema).nullable().defined();
-
-export type Cells = InferType<typeof cellsSchema>;
