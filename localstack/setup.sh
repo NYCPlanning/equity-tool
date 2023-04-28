124dff19c71b905d11cdb96a5e04d95d0db6c237
tangoyankee <miller.tim108@gmail.com>
2023-04-28 13:21:16 -0400

    rename BUCKET to ETL_TAG

--- localstack/setup.sh
+++ localstack/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -z "$BUCKET" ]]; then
-    echo "Missing BUCKET environment variable" 1>&2
+if [[ -z "$ETL_TAG" ]]; then
+    echo "Missing ETL_TAG environment variable" 1>&2
     exit 1
 fi
 
-awslocal s3 mb s3://configs/$BUCKET
-awslocal s3 cp /var/lib/localstack/configs/$BUCKET s3://configs/$BUCKET --recursive
+awslocal s3 mb s3://configs/$ETL_TAG
+awslocal s3 cp /var/lib/localstack/configs/$ETL_TAG s3://configs/$ETL_TAG --recursive
